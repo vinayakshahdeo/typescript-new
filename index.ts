653ca36c2124522b81fd79b8fc7dd44bf9276b7e
vinayakshahdeo <vinushahdeo3333@gmail.com>
2025-09-25 22:34:02 +0530

    test for local runner

--- index.ts
+++ index.ts
@@ -3,5 +3,5 @@ console.log('hello worlds');
 const students = [{ name: 'John', score: 70 }, { name: 'Mark', score: 90 }];
 for (const student of students) {
 	console.log("Name:", student.name);
-	console.log("Score:", student.grade);
+	console.log("Score:", student.score);
 }
