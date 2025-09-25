d62b67700348de4f08cc313bce94e0a12bd78c46
vinayakshahdeo <vinushahdeo3333@gmail.com>
2025-09-25 21:04:00 +0530

    test

---
+++ index.ts
@@ -0,0 +1,7 @@
+console.log('hello worlds');
+
+const students = [{ name: 'John', score: 70 }, { name: 'Mark', score: 90 }];
+for (const student of students) {
+	console.log("Name:", student.name);
+	console.log("Score:", student.grade);
+}
