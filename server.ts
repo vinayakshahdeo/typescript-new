3f14ae3fd79b55604cd1ac10fccdfa58dbc165e4
vinayakshahdeo <vinushahdeo3333@gmail.com>
2025-10-31 22:52:10 +0530

    added type aliases

--- server.ts
+++ server.ts
@@ -1,12 +1,17 @@
 /* eslint-disable prefer-const */
 /* eslint-disable @typescript-eslint/no-unused-vars */
+/*any*/
+
 let name: any = 'vinayak';
 name = 123;
 name = [];
 console.log(name);
-function returnParam(param) {//Parameter 'param' implicitly has an 'any' type.
-	return param;
-}
+
+/*unknown*/
+
+// function returnParam(param) {//Parameter 'param' implicitly has an 'any' type.
+// 	return param;
+// }
 
 function multiplyByTwo(number: unknown) {
 	if (typeof number === 'number') {
@@ -17,3 +22,22 @@ function multiplyByTwo(number: unknown) {
 
 console.log(multiplyByTwo(8));
 console.log(multiplyByTwo('hello'));
+
+/*type aliases*/
+
+type CustomString = string;
+type CustomNumber = number;
+type CustomDate = Date;
+type CustomSymbol = symbol;
+
+let firstName: CustomString = 'vinayak';//type alias used as typescript converted it to string
+console.log(firstName);
+
+let age: CustomNumber = 39;//type alias used as typescript converted it to number
+console.log(age);
+
+let today: CustomDate = new Date();//type alias used as typescript converted it to Date
+console.log(today);
+
+let sym: CustomSymbol = Symbol('my symbol');//type alias used as typescript converted it to symbol
+console.log(sym);
