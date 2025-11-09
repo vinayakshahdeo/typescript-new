b2cb11700399670e81477d6f49ad49458ae8190e
vinayakshahdeo <vinushahdeo3333@gmail.com>
2025-11-09 16:23:11 +0530

    feat: enhance function definitions and add optional parameters in TypeScript

--- functions.ts
+++ functions.ts
@@ -14,3 +14,45 @@ function join(separator: string): (a: string[]) => string {
 		return stringArray.join(separator);
 	};
 }
+
+/* Named function in TypeScript */
+// function intro(name: string, age: number): string {
+// 	return `My name is ${name} and I am ${age} years old.`;
+// }
+/* Function Expression*/
+// const intro2 = function (name: string, age: number): string {
+// 	return `My name is ${name} and I am ${age} years old.`;
+// };
+/* Arrow function */
+// const intro3 = (name: string, age: number): string => {
+// 	return `My name is ${name} and I am ${age} years old.`;
+// };
+
+// Function with Optional Parameter
+// Optional parameters can only be declared after all default parameters are declared
+// function intro(name?: string, age: number): string {
+
+function intro(name: string, age: number, country?: string): string {
+	let countryInfo = country ? ` from ${country}` : '';
+	return `My name is ${name} and I am ${age} years old${countryInfo}.`;
+}
+
+//  custom parameter types and return types for functions
+
+
+enum AgeUnit {
+	Years = 'years',
+	Months = 'months',
+}
+type Person = { name: string; age: number, ageUnit: AgeUnit; }; // type alias
+
+const person1: Person = { name: 'Scott', age: 30, ageUnit: AgeUnit.Years };
+
+function convertAgeToMonths(person: Person): Person {
+	if (person.ageUnit === AgeUnit.Years) {
+		person.age = person.age * 12;
+		person.ageUnit = AgeUnit.Months;
+	}
+	return person;
+}
+console.log(convertAgeToMonths(person1));
