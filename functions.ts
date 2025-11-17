e61aec68c41480b5722cd3153484ccac5def3d8c
vinayakshahdeo <vinushahdeo3333@gmail.com>
2025-11-17 19:08:43 +0530

    removed test case and determined husky is unable to stop typescript error files to be committed

--- functions.ts
+++ functions.ts
@@ -39,7 +39,6 @@ function intro(name: string, age: number, country?: string): string {
 
 //  custom parameter types and return types for functions
 
-asd;
 enum AgeUnit {
 	Years = 'years',
 	Months = 'months',
