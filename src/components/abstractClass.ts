a1f5d6fea0325aaee1e26977ffce5935fa28d82b
vinayakshahdeo <vinushahdeo3333@gmail.com>
2026-01-15 10:23:36 +0530

    ```add class AdminDepartment that extends from Department```

--- src/components/abstractClass.ts
+++ src/components/abstractClass.ts
@@ -1,21 +1,31 @@
 /* Abstract Classes */
 //abstract classes cant be initialized directly
-abstract class Department {
-	constructor(protected name: string) { }
 
-	describe(this: Department) {
-		return `Department: ${this.name}`;
-	}
+type Holidays = { date: Date; reason: string };
+
+abstract class Department {
+  protected abstract holidays: Holidays;
+  constructor(protected name: string) {}
+
+  describe(this: Department) {
+    return `Department: ${this.name}`;
+  }
 }
 
 // const department = new Department('Accounting'); // Error: Cannot create an instance of an abstract class.
 
 class ITDepartment extends Department {
-	constructor() {
-		super('IT');
-	}
-
+  protected holidays: Holidays = { date: new Date('2024-12-25'), reason: 'Christmas' };
+  constructor() {
+    super('IT');
+  }
 }
 
 const ITDepartmentInstance = new ITDepartment();
 ITDepartmentInstance.describe(); // Output: Department: IT
+class AdminDepartment extends Department {
+  protected holidays: Holidays = { date: new Date('2024-01-01'), reason: 'New Year' };
+  constructor() {
+    super('Admin');
+  }
+}
