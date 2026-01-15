4fa865d13e75f394708136074e46214fb44aa9eb
vinayakshahdeo <vinushahdeo3333@gmail.com>
2026-01-15 11:52:50 +0530

    Added interface definition for src/components/interfaces.ts

---
+++ src/components/interfaces.ts
@@ -0,0 +1,10 @@
+/* Interface */
+// interfaces are binding contracts between two components
+// they define the structure of an object
+// they are used to define the shape of an object
+// they are used to define the shape of a function
+// they are used to define the shape of a class
+
+//interfaces are similar to type aliases but they can be extended and implemented
+// interfaces can only be used to define the shape of an object or a function
+// type aliases can be used to define the shape of any type
