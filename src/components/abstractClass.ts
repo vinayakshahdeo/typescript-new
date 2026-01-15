5f2b0e61d21415d0c08dcae3e52acbe5f74134cc
vinayakshahdeo <vinushahdeo3333@gmail.com>
2026-01-15 10:35:53 +0530

    Add abstract class `Holidays` and its implementation in `Department`

--- src/components/abstractClass.ts
+++ src/components/abstractClass.ts
@@ -1,7 +1,7 @@
 /* Abstract Classes */
 //abstract classes cant be initialized directly
 
-type Holidays = { date: Date; reason: string };
+type Holidays = { date: Date; reason: string }[];
 
 abstract class Department {
   protected abstract holidays: Holidays;
@@ -10,22 +10,27 @@ abstract class Department {
   describe(this: Department) {
     return `Department: ${this.name}`;
   }
-}
 
+  public addHolidays(holidays: Holidays) {
+    if (Array.isArray(holidays)) {
+      for (const holiday of holidays) {
+        this.holidays.push(holiday);
+      }
+    }
+  }
+}
 // const department = new Department('Accounting'); // Error: Cannot create an instance of an abstract class.
 
 class ITDepartment extends Department {
-  protected holidays: Holidays = { date: new Date('2024-12-25'), reason: 'Christmas' };
+  protected holidays: Holidays = [];
   constructor() {
-    super('IT');
+    super('IT Department');
   }
 }
-
-const ITDepartmentInstance = new ITDepartment();
-ITDepartmentInstance.describe(); // Output: Department: IT
 class AdminDepartment extends Department {
-  protected holidays: Holidays = { date: new Date('2024-01-01'), reason: 'New Year' };
+  protected holidays: Holidays = [];
   constructor() {
-    super('Admin');
+    super('Admin Department');
   }
 }
+const itDepartment = new ITDepartment();
