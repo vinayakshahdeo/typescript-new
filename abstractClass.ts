e573fc90d909dcc4994bbc1aea764cc31231a777
vinayakshahdeo <vinushahdeo3333@gmail.com>
2026-01-15 09:54:41 +0530

    Add abstract classes

--- abstractClass.ts
+++ abstractClass.ts
@@ -1,1 +1,21 @@
 /* Abstract Classes */
+//abstract classes cant be initialized directly
+abstract class Department {
+	constructor(protected name: string) { }
+
+	describe(this: Department) {
+		console.log(`Department: ${this.name}`);
+	}
+}
+
+// const department = new Department('Accounting'); // Error: Cannot create an instance of an abstract class.
+
+class ITDepartment extends Department {
+	constructor() {
+		super('IT');
+	}
+
+}
+
+const ITDepartmentInstance = new ITDepartment();
+ITDepartmentInstance.describe(); // Output: Department: IT
