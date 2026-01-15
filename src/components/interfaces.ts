881eb06e55aecf0df5982122e1178f793854c34c
vinayakshahdeo <vinushahdeo3333@gmail.com>
2026-01-15 12:17:49 +0530

    Add user interface, implement login/logout and auth functionality.

--- src/components/interfaces.ts
+++ src/components/interfaces.ts
@@ -8,3 +8,48 @@
 //interfaces are similar to type aliases but they can be extended and implemented
 // interfaces can only be used to define the shape of an object or a function
 // type aliases can be used to define the shape of any type
+interface User {
+  username: string;
+  email: string;
+  login(): void;
+  logout(): void;
+}
+class Admin implements User {
+  constructor(
+    public username: string,
+    public email: string,
+    public adminLevel: number
+  ) {}
+  login() {
+    console.log(`${this.username} who is an Admin has logged in`);
+  }
+  logout() {
+    console.log(`${this.username} who is an Admin has logged out`);
+  }
+}
+class Customer implements User {
+  constructor(
+    public username: string,
+    public email: string,
+    public purchaseHistory: string[]
+  ) {}
+  login() {
+    console.log(`${this.username} has logged in`);
+  }
+  logout() {
+    console.log(`${this.username} has logged out`);
+  }
+}
+
+class Auth {
+  static login(user: User) {
+    user.login();
+  }
+  static logout(user: User) {
+    user.logout();
+  }
+}
+const admin = new Admin('admin', 'admin@abc.com', 10);
+Auth.login(admin);
+const customer = new Customer('john_doe', 'johndoe@gmail.com', ['item1', 'item2']);
+Auth.login(customer);
