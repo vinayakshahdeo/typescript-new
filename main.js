efc7fb3d806364597473a142d4288f3c28b8be23
vinayakshahdeo <vinushahdeo3333@gmail.com>
2026-01-17 22:32:48 +0530

    Improve printAuthors method in Book object, using arrow function which doesn't have its own this, instead referencing the parent scope's this (book).

--- main.js
+++ main.js
@@ -9,8 +9,14 @@ const book = {
   authors: ['John Doe', 'Jane Smith'],
   printAuthors() {
     this.authors.forEach(function (author) {
-      console.log(author); //refers to global object
+      console.log(this.title, ' - ', author); //refers to undefined object when this is not passed as Arg to forEach function
+    }, this); //passing this as Arg to forEach
+  },
+  printAuthorsArrow() {
+    this.authors.forEach((author) => {
+      console.log(this.title, ' - ', author); //refers to book object as arrow function doesnt have its own thisitsmaking use of closure of parent scope
     });
   },
 };
 book.printAuthors(); //refers to author values inside forEach
+book.printAuthorsArrow(); //refers to author values inside forEach
