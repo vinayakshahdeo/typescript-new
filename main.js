502474a3f4a2b021596114523baf51a1addbc7ec
vinayakshahdeo <vinushahdeo3333@gmail.com>
2026-01-17 23:18:46 +0530

    Refactor the code by using the Object Literal syntax instead of the Object.create() function to create an object. Also add property descriptors for defining the value, writable, enumerable, and configurable properties of a property. Finally, refactor the code by using classes as syntactic sugar over prototype inheritance to define objects with methods and properties.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 /* this keyword always refers to current object */
-/* this keywordis executed function it would poin to windows orglobal object */
+/* this keywordis executed function it would point to windows orglobal object */
 
 function print(...args) {
   console.log(...args); //refers to global object window in browsers
@@ -56,14 +56,14 @@ function Person(names, email) {
   this.email = email;
   this.points = 0;
   //   this.login = () => {
-  //     console.log(this.names, ' has logged in');
+  //     print(this.names, ' has logged in');
   //   };
   //   this.logout = () => {
-  //     console.log(this.names, ' has logged out');
+  //     print(this.names, ' has logged out');
   //   };
   //   this.addPoints = () => {
   //     this.point++;
-  //     console.log(this.names, ' has ', this.point, ' points');
+  //     print(this.names, ' has ', this.point, ' points');
   //   };
 }
 // const alice = new Person('Alice', 'alice@example.com');
@@ -107,3 +107,84 @@ const superAdmin = new AdminPerson('Super Admin', 'superadmin@example.com', 20);
 superAdmin.login(); //now works as prototype chain is linked
 superAdmin.updateUserReporting(5);
 print(superAdmin);
+
+/* Different ways to create Objects */
+
+//1. Object Literal
+const obj1 = {
+  name: 'Object Literal',
+};
+
+//2. Using Object.create()
+const obj2 = Object.create(obj1);
+obj2.type = 'Created using Object.create()';
+// print(obj2);
+// print(Object.getPrototypeOf(obj2)); //getting prototype of obj2 which is obj1
+
+//3. Using Constructor Function
+function Obj3(name) {
+  this.name = name;
+}
+const obj3 = new Obj3('Created using Constructor Function');
+// print(obj3);
+// print(Object.getPrototypeOf(obj3)); //getting prototype of obj3 which is Obj3.prototype
+
+//4. Using Class
+class Obj4 {
+  constructor(name) {
+    this.name = name;
+  }
+}
+const obj4 = new Obj4('Created using Class');
+print(obj1, obj2, obj3, obj4);
+
+/* Property descriptors */
+// value, writable, enumerable, configurable
+// value: value of property
+// writable: if true, value can be changed
+// enumerable: if true, property shows up during enumeration/iteration of properties
+// configurable: if true, property can be deleted or changed
+
+const books = {
+  title: 'JavaScript Basics',
+  author: 'John Doe',
+  pages: 300,
+};
+
+print(Object.getOwnPropertyDescriptors(books));
+
+/* Defining property Descriptor */
+
+const magazine = new Object();
+
+Object.defineProperty(magazine, 'title', {
+  value: 'Tech Monthly',
+  writable: true,
+  enumerable: true,
+  configurable: true,
+});
+
+print(Object.getOwnPropertyDescriptors(magazine));
+magazine.title = 'New Title'; //will  change as writable is true so error in strict mode
+print(magazine.title);
+Object.defineProperty(magazine, 'author', {
+  value: 'John Smith',
+  writable: false,
+  enumerable: true,
+  configurable: true,
+});
+// magazine.author = 'New Author'; //will not change as writable is false so no error in strict mode
+print(magazine.author);
+
+Object.defineProperty(magazine, 'visited', {
+  value: ['January', 'February', 'March'],
+  writable: true,
+  enumerable: false,
+  configurable: true,
+});
+// print(Object.keys(magazine)); //visited should not show up as enumerable is false butshowing in browser console
+// for (let key in magazine) {
+//   print(key); //visited should not show up as enumerable is false but showing in browser console
+// }
+
+/* Classes are syntactic sugar over protype inheritance. They provide a way to define objects with methods and properties, which can be used to create instances of the object. */
