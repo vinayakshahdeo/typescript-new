053e9c695f027e741a94d165b4458f2812fac931
vinayakshahdeo <vinushahdeo3333@gmail.com>
2026-01-21 13:04:53 +0530

    Refactor utility types to use Partial and exclude generic type for new conditions, updating existing function with improved logging.

--- src/components/utility.ts
+++ src/components/utility.ts
@@ -3,3 +3,20 @@
 function print<T extends unknown[]>(...params: T): void {
   console.log(...params);
 }
+
+/* Partial Types */
+
+/* this add optional condition in all the fields of type */
+
+type Partials<T> = {
+  [P in keyof T]?: T[P];
+};
+
+type Excludes<T, U> = T extends U ? never : T;
+// Now if T has any type which is assignable to U then it will be excluded from T
+interface User {
+  name: string;
+  age: number;
+}
+const user1: Partials<User> = { name: 'Vinayak' };
+print(user1);
