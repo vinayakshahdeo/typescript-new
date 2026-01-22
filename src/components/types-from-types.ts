946c21b2d01c03f32245ed3fd65de0c4c4e7b7aa
vinayakshahdeo <vinushahdeo3333@gmail.com>
2026-01-22 11:31:23 +0530

    Introduce interface Box and method lengthOfContent

--- src/components/types-from-types.ts
+++ src/components/types-from-types.ts
@@ -5,3 +5,32 @@ function print<T extends unknown[]>(...params: T): void {
 // function print<T extends unknown[]>(...params: T): T {
 // 	return params;
 // }
+
+interface Box<Type> {
+  content: Type;
+  size: number;
+}
+
+const stringBox: Box<string> = {
+  content: 'Hello, TypeScript!',
+  size: 10,
+};
+
+const numberBox: Box<number> = {
+  content: 42,
+  size: 5,
+};
+
+interface LengthWise {
+  length: number;
+}
+
+// function lengthOfContent<T>(item: T): void {
+// 	print(item.length);Property 'length' does not exist on type 'T'.
+// }
+
+function lengthOfContent<T extends LengthWise>(item: T): void {
+  print(item.length);
+}
+lengthOfContent(stringBox.content);
+// lengthOfContent(numberBox.content);Argument of type 'number' is not assignable to parameter of type 'LengthWise'.
