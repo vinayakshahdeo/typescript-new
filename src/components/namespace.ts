e47c1a30500febaaf27cf4712479975ab31634bc
vinayakshahdeo <vinushahdeo3333@gmail.com>
2026-01-22 13:17:28 +0530

    add type guard to namespace function

--- src/components/namespace.ts
+++ src/components/namespace.ts
@@ -2,6 +2,6 @@ function prints<T extends unknown[]>(...params: T): void {
   console.log(...params);
 }
 
-// function print<T extends unknown[]>(...params: T): T {
+// function prints<T extends unknown[]>(...params: T): T {
 // 	return params;
 // }
