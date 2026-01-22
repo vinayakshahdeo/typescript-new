a4925c73d16afc1b3ab55e5de72cb99587fc9b1d
vinayakshahdeo <vinushahdeo3333@gmail.com>
2026-01-22 12:53:20 +0530

    add type guard function

--- src/components/typeguard.ts
+++ src/components/typeguard.ts
@@ -5,3 +5,21 @@ function print<T extends unknown[]>(...params: T): void {
 // function print<T extends unknown[]>(...params: T): T {
 // 	return params;
 // }
+
+/* Type Guards */
+
+/* Type guards are used to narrow down the type of a variable within a conditional block */
+
+function printStrings(string: string | string[] | null): void {
+  if (typeof string === 'string') {
+    print('Single string:', string);
+  } else if (Array.isArray(string)) {
+    print('Array of strings:', string.join(', '));
+  } else {
+    print('No string provided');
+  }
+}
+
+printStrings('Hello, Type Guards!');
+printStrings(['Hello', 'Type', 'Guards']);
+printStrings(null);
